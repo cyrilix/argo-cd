47196a3f8d49af03abfbce43fe29f206cc847c3f
Cyrille Nofficial <cynoffic@cyrilix.fr>
2020-03-08 00:54:21 +0100

    [docker] Build multiarch images

--- hack/installers/install-helm2-linux.sh
+++ hack/installers/install-helm2-linux.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -eux -o pipefail
 
-[ -e $DOWNLOADS/helm2.tar.gz ] || curl -sLf --retry 3 -o $DOWNLOADS/helm2.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.15.2-linux-amd64.tar.gz
+TARGETPLATFORM=$1
+
+if [[ -z "${TARGETPLATFORM}" ]]
+then
+  TARGETPLATFORM="linux/amd64"
+fi
+
+OS=$(echo $TARGETPLATFORM | cut -f1 -d/)
+ARCH=$(echo $TARGETPLATFORM | cut -f2 -d/)
+ARM=$(echo $TARGETPLATFORM | cut -f3 -d/ | sed "s/v//" )
+
+[ -e $DOWNLOADS/helm2.tar.gz ] || curl -sLf --retry 3 -o $DOWNLOADS/helm2.tar.gz https://get.helm.sh/helm-v2.15.2-${OS}-${ARCH}.tar.gz
 mkdir -p /tmp/helm2 && tar -C /tmp/helm2 -xf $DOWNLOADS/helm2.tar.gz
-cp /tmp/helm2/linux-amd64/helm $BIN/helm2
+cp /tmp/helm2/${OS}-${ARCH}/helm $BIN/helm2
 helm2 version --client
--- hack/installers/install-kustomize-linux.sh
+++ hack/installers/install-kustomize-linux.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -eux -o pipefail
 
-KUSTOMIZE_VERSION=${KUSTOMIZE_VERSION:-3.2.1}
+TARGETPLATFORM=$1
+
+if [[ -z "${TARGETPLATFORM}" ]]
+then
+  TARGETPLATFORM="linux/amd64"
+fi
+
+OS=$(echo $TARGETPLATFORM | cut -f1 -d/)
+ARCH=$(echo $TARGETPLATFORM | cut -f2 -d/)
+ARM=$(echo $TARGETPLATFORM | cut -f3 -d/ | sed "s/v//" )
+
+KUSTOMIZE_VERSION=${KUSTOMIZE_VERSION:-3.2.0}
 DL=$DOWNLOADS/kustomize-${KUSTOMIZE_VERSION}
 
 # Note that kustomize release URIs have changed for v3.2.1. Then again for
@@ -12,14 +23,32 @@ DL=$DOWNLOADS/kustomize-${KUSTOMIZE_VERSION}
 # v3.3.0 = https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.3.0/kustomize_v3.3.0_linux_amd64.tar.gz
 case $KUSTOMIZE_VERSION in
   2.*)
-    URL=https://github.com/kubernetes-sigs/kustomize/releases/download/v${KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_linux_amd64
+    URL=https://github.com/kubernetes-sigs/kustomize/releases/download/v${KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_${OS}_${ARCH}
     ;;
   *)
-    URL=https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v${KUSTOMIZE_VERSION}/kustomize_kustomize.v${KUSTOMIZE_VERSION}_linux_amd64
+    URL=https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v${KUSTOMIZE_VERSION}/kustomize_kustomize.v${KUSTOMIZE_VERSION}_${OS}_${ARCH}
     ;;
 esac
+
+if [[ "${ARCH}" == "amd64" ]] ; then
 
 [ -e $DL ] || curl -sLf --retry 3 -o $DL $URL
 cp $DL $BIN/kustomize
 chmod +x $BIN/kustomize
 kustomize version
+
+else
+
+  GOPATH=/tmp/go
+  set +e
+  go get github.com/kubernetes-sigs/kustomize
+  set -e
+  cd ${GOPATH}/src/github.com/kubernetes-sigs/kustomize
+  git checkout v${KUSTOMIZE_VERSION}
+  echo "Build ${ARCH} binary"
+  GOARCH=${ARCH} GOOS=${OS} GOARM=${ARM} go build ./cmd/kustomize
+  mv kustomize ${BIN}/kustomize
+  chmod +x $BIN/ks
+  ks version
+
+fi
